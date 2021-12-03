8001015731ee85502a718155633dc32a2bae499f
eeeddddddiiieee <77221274+eeeddddddiiieee@users.noreply.github.com>
2021-12-03 11:19:25 -0600

    Update auto1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto1.java
@@ -13,7 +13,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.teamcode.hwMecanum;
-
+//import deez nuts
 
 
 
