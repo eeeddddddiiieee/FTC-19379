55dac280a7505c313656f9f49d745f96ac9a48ac
eeeddddddiiieee <ezhang@imsa.edu>
2022-01-18 16:36:34 -0600

    Create intakeTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intakeTest.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name="intaketest", group="intaketest")
+
+public class intakeTest extends OpMode {
+    DcMotor intake;
+    public void init(){
+        intake=hardwareMap.dcMotor.get("intake");
+    }
+    public void loop(){
+        intake.setPower(gamepad1.left_trigger-gamepad1.right_trigger);
+    }
+}
