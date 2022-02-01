815a8321e4583bc2db609bcf29bffe94b6131398
eeeddddddiiieee <ezhang@imsa.edu>
2022-02-01 17:10:44 -0600

    j

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedMain.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.te.vision;
+
+
+@Autonomous(name="Redautoprimary",group = "drive")
+public class RedMain extends LinearOpMode {
+    public hwMecanum robot;
+    public lift robotlift;
+    public vision vision1;
+    public static final double ticksPerInch=537.7/11.87373601358268;
+    public depositStateMachine deposit1;
+
+    public void runOpMode() throws InterruptedException{
+
+        initialize();
+        deposit1=new depositStateMachine();
+
+
+        Trajectory move1 = robot.trajectoryBuilder(new Pose2d(38, -62,Math.toRadians(270)),true)
+
+                .splineTo(new Vector2d(14,-40),Math.toRadians(90))
+                .build();
+
+
+        waitForStart();
+
+
+        while (opModeIsActive()){
+
+
+
+            deposit1.dstate1= depositStateMachine.depositState.HIGH;
+
+            deposit1.deposit(robot);
+
+
+
+        }
+
+
+    }
+
+    public void initialize(){
+        robot = new hwMecanum(hardwareMap);
+        robot.init(hardwareMap);
+        deposit1=new depositStateMachine();
+        deposit1.initDeposit();
+        vision1=new vision();
+        vision1.init(hardwareMap);
+    }
+
+
+}
+
--- meepmeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
+++ meepmeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
@@ -1,6 +1,9 @@
 package com.example.meepmeep;
 
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 import com.noahbres.meepmeep.MeepMeep;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
@@ -8,31 +11,89 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import java.util.*;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.SampleMecanumDrive;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
 
 public class MeepMeepTesting {
     public static void main(String args[]){
-        MeepMeep mm = new MeepMeep(600)
-                // Set field image
-                .setBackground(MeepMeep.Background.FIELD_FREIGHT_FRENZY)
-                // Set theme
-                .setTheme(new ColorSchemeRedDark())
-                // Background opacity from 0-1
-                .setBackgroundAlpha(1f)
-                // Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(50, 30, Math.toRadians(180), Math.toRadians(180), 12)
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        RoadRunnerBotEntity redMain = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 12)
+                .setDimensions(12,14)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-31, -63, Math.toRadians(270)))
-                                .setReversed(true)
-                                .splineTo(new Vector2d(-12,-44),Math.toRadians(90))
-                                .waitSeconds(1)
-                                .setReversed(false)
-                                .splineTo(new Vector2d(-54,-54),Math.toRadians(270))
-                                .waitSeconds(1)
-                                .setReversed(true)
-                                .splineToConstantHeading(new Vector2d(-60,-36),Math.toRadians(0))
+                        drive.trajectorySequenceBuilder(new Pose2d(6, -65, Math.toRadians(270)))
+                                .setReversed(TRUE)
+                                .splineTo(new Vector2d(-12,-40),Math.toRadians(90))
+                                .setReversed(FALSE)
+                                .splineToSplineHeading(new Pose2d(14,-65,Math.toRadians(0)),Math.toRadians(-20))
+                                .forward(10)
+                                .lineTo(new Vector2d(44,-65),
+                                        SampleMecanumDrive.getVelocityConstraint(25, 60, 12),
+                                        SampleMecanumDrive.getAccelerationConstraint(20)
+                                )
+                                .back(24)
+                                .setReversed(TRUE)
+                                .splineTo(new Vector2d(-12,-40),Math.toRadians(90))
+
+
+
+
 
                                 .build()
-                )
+                );
+        RoadRunnerBotEntity blueMain = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 12)
+                .setDimensions(12,14)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(6, 65, Math.toRadians(-270)))
+                                .setReversed(TRUE)
+                                .splineTo(new Vector2d(-12,40),Math.toRadians(-90))
+                                .setReversed(FALSE)
+                                .splineToSplineHeading(new Pose2d(14,65,Math.toRadians(-0)),Math.toRadians(20))
+                                .forward(10)
+                                .lineTo(new Vector2d(44,65),
+                                        SampleMecanumDrive.getVelocityConstraint(25, 60, 12),
+                                        SampleMecanumDrive.getAccelerationConstraint(20)
+                                )
+                                .back(24)
+                                .setReversed(TRUE)
+                                .splineTo(new Vector2d(-12,40),Math.toRadians(-90))
+
+                                .build()
+                );
+        RoadRunnerBotEntity red2 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 12)
+                .setDimensions(12,14)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-42, -65, Math.toRadians(270)))
+                                .setReversed(TRUE)
+                                .splineTo(new Vector2d(-12,-40),Math.toRadians(90))
+                                .setReversed(FALSE)
+                                .splineTo(new Vector2d(-60,-60),Math.toRadians(225))
+                                .waitSeconds(1)
+                                .setReversed(TRUE)
+                                .splineToLinearHeading(new Pose2d(-50,-60,Math.toRadians(270)),0)
+
+
+                                .build()
+                );
+
+
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
+
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(red2)
+
                 .start();
+
     }
 }
